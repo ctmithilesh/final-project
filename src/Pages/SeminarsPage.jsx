2370f181a42233b963ed8830236509399b7d0794
mithilesh <mithilesht@cloudthat.com>
2023-10-26 10:33:59 +0530

    fixed page scroll

--- src/Pages/SeminarsPage.jsx
+++ src/Pages/SeminarsPage.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import SideMenu from '../Components/SideMenu'
 import MySeminars from '../Components/MySeminars'
 
 const SeminarsPage = () => {
+
+    useEffect(() => {
+
+        window.scrollTo(0, 0)
+
+    })
+
+
     return (
         <div class="flex flex-no-wrap">
             <div class="min-h-screen border-r border-gray-100 sticky top-0 h-full bg-gray-100 z-20">
